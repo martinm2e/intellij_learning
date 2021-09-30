edad29e88c76345c7ac13e906e74e30b5ca2a8c3
martinm2e <martinm2e@gmail.com>
2021-09-30 22:42:50 +0300

    one more test

--- src/com/company/service/Main.java
+++ src/com/company/service/Main.java
@@ -10,6 +10,7 @@ public class Main {
         System.out.println("Hello world");
         System.out.println("Hello world 2");
         System.out.println("Hello world 3");
+        System.out.println("Hello world 4");
 
         Product product;
         product = new Product("Keyboard");
